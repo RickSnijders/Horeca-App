6ea0879a31ee32a39ba8c1486b0f92b75d75e5a9
RickSnijders <99060582@mydavinci.nl>
2019-10-30 12:07:55 +0100

    Horeca app af

--- HorecaApp.js
+++ HorecaApp.js
@@ -1,29 +1,50 @@
 var loop = false
 var loop2 = false
 var loop3 = false
+var loop4 = false
 const prijsFris = 2
 const prijsBier = 2.5
 const prijsWijn = 4
+const prijsBal8 = 5
+const prijsBal16 = 9
 var aantalFris = 0
 var aantalBier = 0
 var aantalWijn = 0
-
+var aantalBal8 = 0
+var aantalBal16 = 0
+var bestellingFris = 0
+var bestellingBier = 0
+var bestellingWijn = 0
+var bestellingBitterbal = 0
+var totaalAantal = 0
+var totaalPrijs = 0
+var geenBestelling = 0
+var totalPriceFris = 0
+var totalPriceBier = 0
+var totalPriceWijn = 0
+var totalPriceBal = 0
+var bitterbal8 = 0
+var bitterbal16 = 0
 
 
 	while(loop == false){
-		var bestelling = prompt("Welke bestelling wilt u toevoegen? \n\nKeuze uit: fris, bier of wijn \n\nGerbuik 'stop' om af te rekenen").toLowerCase();
+		var bestelling = prompt("Welke bestelling wilt u toevoegen? \n\nKeuze uit: fris, bier, wijn of snack \n\nGerbuik 'stop' om af te rekenen").toLowerCase();
 		switch(bestelling){
+
 
 			case "fris":
 
 				loop2=false
 				while(loop2 == false){	
 					var bestellingFris = prompt("Hoeveel fris wilt u toevoegen aan uw bestelling?");
-					if(isNaN(bestellingFris)){
+					if(isNaN(bestellingFris) || bestellingFris == 0){
 						alert("Dit is geen geldig karakter.");
+						bestellingFris = 0			// Dit zet het aantal besteld naar 0 als er geen geldig karakter is gebruikt
 					}
 					else{
 						loop2= true
+						geenBestelling = 1	//De var geenBestelling wordt naar 1 gezet omdat er een bestelling is toegevoegd, als geenBestelling 0 is is er niks besteld
+						
 					}
 					
 					aantalFris= +aantalFris + +bestellingFris
@@ -32,7 +53,7 @@ var aantalWijn = 0
 
 				}
 
-
+				
 
 				break;
 			
@@ -41,15 +62,18 @@ var aantalWijn = 0
 				loop2=false
 				while(loop2 == false){	
 					var bestellingBier = prompt("Hoeveel bier wilt u toevoegen aan uw bestelling?");
-					if(isNaN(bestellingBier)){
+					if(isNaN(bestellingBier) || bestellingBier == 0){
 						alert("Dit is geen geldig karakter.");
+						bestellingBier = 0
 					}
 					else{
 						loop2= true
+							geenBestelling = 1
+						
 					}
 					aantalBier= +aantalBier + +bestellingBier
 					totalPriceBier= prijsBier * aantalBier
-					document.getElementById("bier").innerHTML = "&#8364; " + totalPriceBier
+					document.getElementById("bier").innerHTML = aantalBier+ " x Bier" + " &#8364; " + totalPriceBier
 				}
 
 
@@ -57,31 +81,99 @@ var aantalWijn = 0
 				break;
 
 			case "wijn":
-
-				loop2=false
-				while(loop2 == false){	
+				loop4=false
+				while(loop4 == false){	
 					var bestellingWijn = prompt("Hoeveel wijn wilt u toevoegen aan uw bestelling?");
-					if(isNaN(bestellingWijn)){
+					if(isNaN(bestellingWijn) || bestellingWijn == 0){
 						alert("Dit is geen geldig karakter.");
+						bestellingWijn = 0 	
 					}
 					else{
-						loop2= true
+						loop4= true
+							geenBestelling = 1;
+	
 					}
 					aantalWijn= +aantalWijn + + bestellingWijn
 					totalPriceWijn= prijsWijn * aantalWijn
-					document.getElementById("wijn").innerHTML = "&#8364; " + totalPriceWijn
+					document.getElementById("wijn").innerHTML = aantalWijn+ " x Wijn" + " &#8364; " + totalPriceWijn
 				}
-
-
-
 				break;
+			
+			case "snack":
+				loop2=false
+				while(loop2 == false){	
+					var bestellingBitterbal = prompt("Hoeveel bitterballen wilt u toevoegen (8 of 16)?");
+					
+					if(bestellingBitterbal == 8){
+						loop2= true
+						geenBestelling = 1
+						bestellingBitterbal = bitterbal8
+						loop3 = false
+						while(loop3 == false){
+							var bitterbal8 = prompt("Hoeveel bitterbalschalen van 8 stuks wilt u bestellen?");
+							if(isNaN(bitterbal8)){
+								alert("Dit is geen geldig karakter")
+							}
+							else{
+								loop3= true
+								geenBestelling = 1
+								document.getElementById("bitterbal8").style.display = "block";
+							
+							}
 
-			case "stop":
-				loop = true
+						}
+					}
+
+					else if(bestellingBitterbal ==16){
+						loop2=true
+						geenBestelling = 1
+						bestellingBitterbal = bitterbal16
+						loop3= false
+						while(loop3 == false){
+							var bitterbal16 = prompt("Hoeveel bitterbalschalen van 16 stuks wilt u bestellen?");
+							if(isNaN(bitterbal16)){
+								alert("Dit is geen geldig karakter")
+							}
+							else{
+								loop3= true
+								geenBestelling = 1	
+								document.getElementById("bitterbal16").style.display = "block";
+
+							}
+						}
+					}
+					
+
+					else {
+						alert("U kunt alleen een keuze maken tussen 8 en 16. De snacks zijn niet toegevoegd aan de bestelling.")
+						bestellingBitterbal = 0
+					}
+					
+					aantalBal8= +aantalBal8 + + bitterbal8
+					aantalBal16= +aantalBal16 + + bitterbal16
+					totalPriceBal8 = prijsBal8 * aantalBal8
+					totalPriceBal16 = prijsBal16 * aantalBal16
+					document.getElementById("bitterbal8").innerHTML = aantalBal8+ " x Bitterbal 8" + " &#8364; " + totalPriceBal8
+					document.getElementById("bitterbal16").innerHTML = aantalBal16+ " x Bitterbal 16" + " &#8364; " + totalPriceBal16
+				}
 				
 				break;
 
-			default: alert("dus smeck haar naar rechts");
+			case "stop":
+				var totaalAantal = +aantalFris + +aantalBier + +aantalWijn + +aantalBal8 + +aantalBal16
+				var totaalPrijs = +totalPriceFris + +totalPriceBier + +totalPriceWijn + + totalPriceBal8 + +totalPriceBal16
+				if(geenBestelling < 1){
+				alert("U heeft nog niks besteld");
+					loop = false
+				}
+
+				else{
+					loop = true
+					document.getElementById("totaal").innerHTML =  totaalAantal + " Totaal: &#8364 "+ totaalPrijs
+				}
+				break;
+				
+			default: alert("U heeft een ongeldige invoer gedaan. Uw bestelling kan niet worden toegevoegd.");
 			break;
-		}
+		}	
 	}
